c2aab1dca0be0c3b010ac789f35444d9ec199b6e
seolyam <leeyam.dev@gmail.com>
2024-12-26 15:31:41 +0800

    chore: fix footerpage import

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import EventsPage from "./events/page";
 import HeroPage from "./components/hero/page";
 import Navbar from "./components/navbar/page";
-import { FooterPage } from "./components/footer/page";
+import FooterPage from "./components/footer/page";
 
 export default function Home() {
   return (
