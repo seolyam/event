4de73ae6e3a0dcc4be0420730c7ae077a52dbd50
seolyam <leeyam.dev@gmail.com>
2024-12-28 13:06:37 +0800

    chore: add offers

---
+++ app/components/offers/page.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Offers = () => {
+  return (
+    <div className="flex justify-center">
+      <div className=" rounded-lg p-6 shadow-2xl w-fit scale-110">
+        <div className="text-3xl ">
+          As part of the global GDG network, the chapter offers <br />{" "}
+          <span className="text-[#4385f3]"> exclusive opportunities</span> to
+          engage with Google’s tools and <br /> platforms through
+          <span className="text-[#32A951]"> workshops</span>,
+          <span className="text-[#FCBC05]"> tech talks </span>, and{" "}
+          <span className="text-[#E94335]">
+            collaborative
+            <br /> projects
+          </span>
+          .
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Offers;
