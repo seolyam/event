7616a476f6dcade2ed6efb0bee5460c9becb8916
seolyam <leeyam.dev@gmail.com>
2024-12-28 13:08:05 +0800

    chore: update routing

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,17 @@ import EventsPage from "./events/page";
 import HeroPage from "./components/hero/page";
 import Navbar from "./components/navbar/page";
 import FooterPage from "./components/footer/page";
+import ProjectPage from "./projects/page";
+import Offers from "./components/offers/page";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
       <HeroPage />
+      <Offers />
       <EventsPage />
+      <ProjectPage />
       <FooterPage />
     </div>
   );
