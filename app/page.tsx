c9a2650a230801d0c7a57d0601fdf5313109a0d4
seolyam <leeyam.dev@gmail.com>
2024-12-28 13:16:12 +0800

    chore: switch event and project order

--- app/page.tsx
+++ app/page.tsx
@@ -11,8 +11,8 @@ export default function Home() {
       <Navbar />
       <HeroPage />
       <Offers />
-      <EventsPage />
       <ProjectPage />
+      <EventsPage />
       <FooterPage />
     </div>
   );
