382ecf3905e6d0b2c7fb35e591272db9c155a314
seolyam <leeyam.dev@gmail.com>
2025-01-02 16:04:42 +0800

    chore: refactor layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,47 +2,38 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "./components/navbar/page";
 import FooterPage from "./components/footer/page";
-import Head from "next/head";
+
+const description =
+  "Google Developer Groups on Campus - Empowering developers through events and projects";
+const imgUrl = "/images/og-image.jpg";
 
 export const metadata: Metadata = {
   title: "GDGOC Events",
-  description:
-    "Google Developer Groups on Campus - Empowering developers through events and projects",
+  description,
   metadataBase: new URL("https://gdgoc-event.vercel.app"),
   openGraph: {
-    title: "GDGOC Events",
-    description:
-      "Google Developer Groups on Campus - Empowering developers through events and projects",
+    type: "website",
     url: "https://gdgoc-event.vercel.app",
+    title: "GDGOC Events",
+    description,
     siteName: "GDGOC Events",
     images: [
       {
-        url: "/images/og-image.jpg",
+        url: imgUrl,
         width: 1200,
         height: 630,
         alt: "GDGOC Events Landing Page",
       },
     ],
-    locale: "en_US",
-    type: "website",
   },
   twitter: {
     card: "summary_large_image",
     title: "GDGOC Events",
-    description:
-      "Google Developer Groups on Campus - Empowering developers through events and projects",
-    images: ["/images/og-image.jpg"],
-  },
-  robots: {
-    index: true,
-    follow: true,
+    description,
+    images: [imgUrl],
   },
   icons: {
-    icon: [
-      { url: "/favicon.ico", sizes: "any" },
-      { url: "/icon.svg", type: "image/svg+xml" },
-    ],
-    apple: "/apple-touch-icon.png",
+    icon: "/favicon.ico",
   },
 };
 
@@ -53,9 +44,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-      </Head>
       <body className="font-sans min-h-screen bg-white text-gray-900">
         <Navbar />
         {children}
