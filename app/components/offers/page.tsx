953857d580c899495b932ad15d168fe50adc3a45
seolyam <leeyam.dev@gmail.com>
2025-01-02 14:17:00 +0800

    chore: make mobile view responsive

--- app/components/offers/page.tsx
+++ app/components/offers/page.tsx
@@ -2,17 +2,18 @@ import React from "react";
 
 const Offers = () => {
   return (
-    <div className="flex justify-center">
-      <div className=" rounded-lg p-6 shadow-2xl w-fit scale-110">
-        <div className="text-3xl ">
-          As part of the global GDG network, the chapter offers <br />{" "}
-          <span className="text-[#4385f3]"> exclusive opportunities</span> to
-          engage with Google’s tools and <br /> platforms through
-          <span className="text-[#32A951]"> workshops</span>,
-          <span className="text-[#FCBC05]"> tech talks </span>, and{" "}
+    <div className="flex justify-center px-4 py-8 sm:py-0">
+      <div className="rounded-lg p-6 shadow-2xl w-full sm:w-fit sm:scale-110">
+        <div className="text-lg sm:text-3xl leading-relaxed sm:leading-normal">
+          As part of the global GDG network, the chapter offers{" "}
+          <br className="hidden sm:block" />
+          <span className="text-[#4385f3]">exclusive opportunities</span> to
+          engage with Google’s tools <br className="hidden sm:block" /> and
+          platforms through <span className="text-[#32A951]">workshops</span>,{" "}
+          <span className="text-[#FCBC05]">tech talks</span>, and{" "}
           <span className="text-[#E94335]">
             collaborative
-            <br /> projects
+            <br className="hidden sm:block" /> projects
           </span>
           .
         </div>
