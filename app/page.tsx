af5ff55999a339d253454cb73bb0486c9f6867a8
seolyam <leeyam.dev@gmail.com>
2025-01-02 15:00:47 +0800

    chore: remove navbar & footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,15 @@
 import EventsPage from "./events/page";
 import HeroPage from "./components/hero/page";
-import Navbar from "./components/navbar/page";
-import FooterPage from "./components/footer/page";
 import ProjectPage from "./projects/page";
 import Offers from "./components/offers/page";
 
 export default function Home() {
   return (
     <div>
-      <Navbar />
       <HeroPage />
       <Offers />
       <ProjectPage />
       <EventsPage />
-      <FooterPage />
     </div>
   );
 }
